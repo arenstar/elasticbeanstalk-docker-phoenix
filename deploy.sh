9c230ce1ad2f62e517cbc3cd4b403da37746501b
Toby Hede <tobyhede@info-architects.net>
2016-01-15 16:11:46 +1100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 echo $1 > version
-git add .
-git commit -am $1
-eb deploy
+# git add .
+# git commit -am $1
+# eb deploy
+
+git archive --format=zip HEAD > application.zip
+aws s3 cp ./application.zip s3://elasticbeanstalk-us-west-2-hello-phoenix/
+aws elasticbeanstalk create-application-version \
+  --application-name 'Hello Phoenix' \
+  --version-label v$1 \
+  --source-bundle S3Bucket=elasticbeanstalk-us-west-2-hello-phoenix,S3Key=application.zip
